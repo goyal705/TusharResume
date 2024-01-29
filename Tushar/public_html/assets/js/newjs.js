578a19bcf53248f56ac41c1e04e095118e0b9ade
goyal705 <75295353+goyal705@users.noreply.github.com>
2024-01-29 20:46:01 +0530

    Add files via upload

---
+++ Tushar/public_html/assets/js/newjs.js
@@ -0,0 +1,41 @@
+document.getElementById('downloadButton').addEventListener('click', function() {
+    // Create a link element
+    var downloadLink = document.createElement('a');
+
+    // Set the download attribute and file name
+    downloadLink.download = 'tusharresume.pdf';
+
+    // Set the href attribute to the path of the PDF file
+    downloadLink.target="_blank"
+    downloadLink.href = "Tushar's Resume-hackerresume (2).pdf";
+
+    // Append the link to the document
+    document.body.appendChild(downloadLink);
+
+    // Trigger a click event on the link to initiate the download
+    downloadLink.click();
+
+    // Remove the link from the document
+    document.body.removeChild(downloadLink);
+});
+
+document.getElementById('downloadButton2').addEventListener('click', function() {
+    // Create a link element
+    var downloadLink = document.createElement('a');
+
+    // Set the download attribute and file name
+    downloadLink.download = 'tusharresume.pdf';
+
+    // Set the href attribute to the path of the PDF file
+    downloadLink.target="_blank"
+    downloadLink.href = "Tushar's Resume-hackerresume (2).pdf";
+
+    // Append the link to the document
+    document.body.appendChild(downloadLink);
+
+    // Trigger a click event on the link to initiate the download
+    downloadLink.click();
+
+    // Remove the link from the document
+    document.body.removeChild(downloadLink);
+});
